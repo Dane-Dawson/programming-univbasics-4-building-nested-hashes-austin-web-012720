9acdfc220ecfc5ec27f1177ff5f0b5c5b067e969
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-13 22:11:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,10 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :rent_in_dollars => {
-        :rent => {}
+        :level1 => {},
+        :level2 => {},
+        :level3 => {},
+        :level4 => {}
       }
     }
   }
